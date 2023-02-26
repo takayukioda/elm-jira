b3b2b2b5033c4c12adc0c3e2481b41aeea78df0c
takayukioda <oda@sansan.com>
2023-02-26 13:15:43 +0900

    chore: format

--- src/Main.elm
+++ src/Main.elm
@@ -1,118 +1,134 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, blockquote, p, h2, text, pre, cite, button)
-import Html exposing (article)
+import Html exposing (Html, article, blockquote, button, cite, div, h2, p, pre, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, map4, field, int, string)
+import Json.Decode exposing (Decoder, field, int, map4, string)
+
 
 
 -- Main
 
 
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
 
 
 -- Model
 
 
 type Model
-  = Failure
-  | Loading
-  | Success Quote
+    = Failure
+    | Loading
+    | Success Quote
+
 
 type alias Quote =
-  { quote : String
-  , source : String
-  , author : String
-  , year : Int
-  }
+    { quote : String
+    , source : String
+    , author : String
+    , year : Int
+    }
 
-init : () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (Loading , getRandomQuote)
+    ( Loading, getRandomQuote )
+
 
 
 -- Update
 
 
 type Msg
-  = MorePlease
-  | GotQuote (Result Http.Error Quote)
+    = MorePlease
+    | GotQuote (Result Http.Error Quote)
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    MorePlease ->
-      (Loading, getRandomQuote)
-    GotQuote result ->
-      case result of
-        Ok quote ->
-          (Success quote, Cmd.none)
-        Err _ ->
-          (Failure, Cmd.none)
+    case msg of
+        MorePlease ->
+            ( Loading, getRandomQuote )
+
+        GotQuote result ->
+            case result of
+                Ok quote ->
+                    ( Success quote, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
 
 -- Subscriptions
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- View
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [ text "Random Quotes" ]
-    , viewQuote model
-    ]
+    div []
+        [ h2 [] [ text "Random Quotes" ]
+        , viewQuote model
+        ]
+
 
 viewQuote : Model -> Html Msg
 viewQuote model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load a random quote for some reason."
-        , button [ onClick MorePlease ] [ text "Try Again!" ]
-        ]
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random quote for some reason."
+                , button [ onClick MorePlease ] [ text "Try Again!" ]
+                ]
 
-    Loading ->
-      text "Loading..."
+        Loading ->
+            text "Loading..."
 
-    Success quote ->
-      div []
-        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
-        , blockquote [] [ text quote.quote ]
-        , p [ style "text-align" "right" ]
-          [ text "- "
-          , cite [] [ text quote.source ]
-          , text (" by " ++ quote.author ++ " (" ++ String.fromInt quote.year ++ ")")
-          ]
-        ]
+        Success quote ->
+            div []
+                [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+                , blockquote [] [ text quote.quote ]
+                , p [ style "text-align" "right" ]
+                    [ text "- "
+                    , cite [] [ text quote.source ]
+                    , text (" by " ++ quote.author ++ " (" ++ String.fromInt quote.year ++ ")")
+                    ]
+                ]
+
 
 
 -- HTTP
 
+
 getRandomQuote : Cmd Msg
 getRandomQuote =
-  Http.get
-    { url = "https://elm-lang.org/api/random-quotes"
-    , expect = Http.expectJson GotQuote quoteDecoder
-    }
+    Http.get
+        { url = "https://elm-lang.org/api/random-quotes"
+        , expect = Http.expectJson GotQuote quoteDecoder
+        }
+
 
 quoteDecoder : Decoder Quote
 quoteDecoder =
-  map4 Quote
-    (field "quote" string)
-    (field "source" string)
-    (field "author" string)
-    (field "year" int)
+    map4 Quote
+        (field "quote" string)
+        (field "source" string)
+        (field "author" string)
+        (field "year" int)
